ad87e9b83222f28dd8798ee9b8143e78bea11ab6
Drew Chase (Work) <dcmanproductions@gmail.com>
2026-09-17 15:28:49 -0400

    Replace `Home` and `About` with `Login` and `Dashboard`, remove `Navigation` and `ThemeProvider`.

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,14 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import "./css/index.css";
-import Home from "./pages/Home.tsx";
-import About from "./pages/About.tsx";
-import Navigation from "./components/Navigation.tsx";
-import {ThemeProvider} from "./providers/ThemeProvider.tsx";
+import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <ThemeProvider>
                 <MainContentRenderer/>
-            </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
@@ -23,11 +19,10 @@ export function MainContentRenderer()
 {
     return (
         <>
-            <Navigation/>
             <Routes>
                 <Route>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
                 </Route>
             </Routes>
         </>
